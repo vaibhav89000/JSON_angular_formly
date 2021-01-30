4ffd884415a19529f4812a2f6bacf8d1929ad371
vaibhav89000 <vaibhav89000@gmail.com>
2021-01-30 17:44:11 +0530

    form using ts

--- formly/src/app/app.component.ts
+++ formly/src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'formly';
+
+  // form: FormGroup;
+  options: FormlyFormOptions = {};
+  // model: any;
+  // fields: FormlyFieldConfig[];
+
+  form = new FormGroup({});
+  model: any = { };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'FirstName',
+      type: 'input',
+      templateOptions: {
+        label: 'First Name',
+        placeholder: 'First Name',
+        required: false,
+      },
+    },
+    {
+      key: 'LastName',
+      type: 'input',
+      templateOptions: {
+        label: 'Last Name',
+        placeholder: 'Last Name',
+        required: false,
+      },
+    },
+    {
+      key: 'PhoneNumber',
+      type: 'input',
+      templateOptions: {
+        label: 'PhoneNumber',
+        placeholder: 'Phone Number',
+        type: 'Number',
+        required: false,
+      },
+    }
+  ];
+
+
+  ngOnInit(){
+
+  }
+
+  submit(){
+    console.log(this.model);
+  }
+
 }
--- formly/src/app/app.module.ts
+++ formly/src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot(),
+    FormlyModule.forRoot({ extras: { lazyRender: true } }),
   ],
   providers: [],
   bootstrap: [AppComponent]
